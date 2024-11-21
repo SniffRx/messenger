7b218afd5c9b360f5b99c6ca765a5e944769e354
SniffRx <fortik.sindroms@yandex.ru>
2024-11-21 17:34:14 +0300

    Added main file

---
+++ server/src/index.ts
@@ -0,0 +1,54 @@
+import Fastify, {FastifyReply, FastifyRequest} from "fastify"
+import dotenv from 'dotenv'
+import jwt from "@fastify/jwt";
+import fastifyFormbody from '@fastify/formbody'
+import {registerRoutes} from "./routes";
+
+dotenv.config()
+
+export const server = Fastify({
+    trustProxy: true,
+    logger: {
+        level: "warn"
+    }
+})
+
+server.register(jwt, {
+    secret: process.env.JWT_SECRET ?? 'your-secret-key'
+});
+
+server.register(fastifyFormbody);
+
+
+// Расширяем сервер через `declare`
+declare module 'fastify' {
+    interface FastifyInstance {
+        authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
+    }
+}
+
+// Добавляем декоратор `authenticate`
+server.decorate('authenticate', async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+        const token = request.headers.authorization?.replace('Bearer ', '');
+        if (!token) {
+            reply.status(401).send({ error: 'Token is required' });
+            return;
+        }
+        request.user = server.jwt.verify(token) as { userId: number; username: string }; // Сохраняем декодированный токен
+    } catch (err) {
+        reply.status(401).send({ error: 'Invalid or expired token' });
+    }
+});
+
+// Регистрируем маршруты
+registerRoutes(server);
+
+
+server.listen({port:Number(process.env.PORT ?? 3000)}, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
