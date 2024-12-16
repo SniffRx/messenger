04dff46cab8f1f2def7e256e75a236e71fac28cb
SniffRx <fortik.sindroms@yandex.ru>
2024-12-16 18:55:29 +0300

    Added swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,8 @@ import {registerRoutes} from "./routes";
 import "colors.ts";
 import {registerMiddleWare} from "./middleware/middleware";
 import cors from '@fastify/cors';
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 dotenv.config()
 
@@ -26,11 +28,50 @@ server.register(jwt, {
     secret: process.env.JWT_SECRET ?? 'your-secret-key'
 });
 
+// Регистрация Swagger
+server.register(fastifySwagger, {
+    openapi: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Test swagger',
+            description: 'Testing the Fastify swagger API',
+            version: '0.1.0'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+                description: 'Development server'
+            }
+        ],
+        components: {
+            securitySchemes: {
+                apiKey: {
+                    type: 'apiKey',
+                    name: 'apiKey',
+                    in: 'header'
+                }
+            }
+        },
+        externalDocs: {
+            url: 'https://swagger.io',
+            description: 'Find more info here'
+        }
+    }
+});
+
+// Подключение интерфейса Swagger UI
+server.register(fastifySwaggerUi, {
+    routePrefix: "/docs", // Swagger UI будет доступен по пути /docs
+    staticCSP: true,
+    transformSpecification: (swaggerObject, request, reply) => swaggerObject,
+    transformSpecificationClone: true,
+});
+
 server.register(fastifyFormbody);
 
 // Регистрируем маршруты
 registerRoutes(server);
-registerMiddleWare(server)
+registerMiddleWare(server);
 
 server.listen({
     host: process.env.ADDRESS ?? "0.0.0.0",
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,14 +7,26 @@ import {register} from "./register";
 import {confirmFriend} from "./friends/confirm";
 import {createChat} from "./chats/create";
 import websocketRoutes from "./websocket";
+import {getChats} from "./chats/show";
+import {showFriendRequests} from "./friends/showRequests";
+import {sendMessage} from "./chats/send";
+import {getChatMessages} from "./chats/messages";
 
 export async function registerRoutes(server: FastifyInstance) {
     await login(server);
     await register(server);
+
     await showFriends(server);
+    await showFriendRequests(server);
+
     await addFriends(server);
     await removeFriends(server);
     await confirmFriend(server);
+
+    await getChats(server);
     await createChat(server);
+    await sendMessage(server);
+    await getChatMessages(server);
+
     await websocketRoutes(server);
 }
