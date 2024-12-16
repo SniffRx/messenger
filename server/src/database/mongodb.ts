f3fa11dfba7f432e6e6b8f0bb53002891cac9cef
SniffRx <fortik.sindroms@yandex.ru>
2024-12-16 18:55:14 +0300

    Added logging for mongodb.ts

--- server/src/database/mongodb.ts
+++ server/src/database/mongodb.ts
@@ -1,20 +1,21 @@
-import { MongoClient } from 'mongodb'
+import {MongoClient} from 'mongodb'
 import "colors.ts"
 import dotenv from 'dotenv'
+
 dotenv.config(); // Загружает .env
 
-const mongoUrl = ''+process.env.MONGODB_HOST; // URL для подключения к MongoDB
-const dbName = ''+process.env.MONGODB_DATABASE; // Название базы данных
+const mongoUrl = '' + process.env.MONGODB_HOST; // URL для подключения к MongoDB
+const dbName = '' + process.env.MONGODB_DATABASE; // Название базы данных
 
 export const mongoClient = new MongoClient(mongoUrl);
 
-export const connectToMongoDB = async () => {
-    try {
-        await mongoClient.connect();
-        console.log('MongoDB connected'.green);
-    } catch (error) {
-        console.error(`MongoDB connection error: ${error}`.red);
-    }
-};
-
 export const getMongoDb = () => mongoClient.db(dbName);
+
+// Если хотите сразу подключить MongoDB
+mongoClient.connect()
+    .then(() => {
+        console.log('MongoDB connected'.green);
+    })
+    .catch((error) => {
+        console.error(`MongoDB connection error: ${error}`.red);
+    });
